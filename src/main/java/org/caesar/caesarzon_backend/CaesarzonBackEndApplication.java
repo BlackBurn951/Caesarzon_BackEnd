b8254a8471e1db613f5f5e5e7b7e9fac436e4962
BlackBurn951 <aldonocera95@gmail.com>
2024-05-03 10:49:12 +0200

    First commit

---
+++ src/main/java/org/caesar/caesarzon_backend/CaesarzonBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.caesar.caesarzon_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaesarzonBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaesarzonBackEndApplication.class, args);
+    }
+
+}
