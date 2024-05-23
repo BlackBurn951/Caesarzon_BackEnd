a4e6d3865d346e3230d01b061098d55ae36f82eb
Luke6933 <127422711+Luke6933@users.noreply.github.com>
2024-05-23 18:01:02 +0200

    Rivista autorizzazione

---
+++ Auth-Service/src/main/java/org/caesar/authservice/Config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package org.caesar.authservice.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    public static final String ADMIN = "admin";
+    public static final String BASIC = "basic";
+
+    private final JwtConverter jwtConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.POST, "/aidi").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/auth-api/test").hasRole(ADMIN)
+                        .requestMatchers(HttpMethod.GET, "/products/random").hasAnyRole(ADMIN, BASIC)
+                        .requestMatchers(HttpMethod.GET, "/products/{id}").hasRole(BASIC)
+                 .anyRequest().authenticated());
+
+        http.sessionManagement(sess -> sess.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS));
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)));
+
+        return http.build();
+    }
+}
--- Auth-Service/src/main/java/org/caesar/authservice/Controller/AuthController.java
+++ Auth-Service/src/main/java/org/caesar/authservice/Controller/AuthController.java
@@ -1,15 +1,15 @@
 package org.caesar.authservice.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.caesar.authservice.Entities.Tokens;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth-api")
 public class AuthController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "testRiuscito!!!";
+    @PostMapping("/test")
+    public String test(@RequestBody Tokens tokens) {
+        System.out.println(tokens.getAccess());
+        return tokens.getAccess();
     }
 }
